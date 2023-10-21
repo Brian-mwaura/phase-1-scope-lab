d6784a2cd884fbab7b6293e6598721c8ca4f20a4
Brian <briomwaura@gmail.com>
2023-10-21 10:57:58 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName; // Declaring a variable in global scope
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase(); // Accessing and modifying the global variable
+  }
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Declaring a global variable from inside a function
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'someone else'; // Modifying the global variable
+}
+
+const leastFavoriteCustomer = 'initial value'; // Declaring a constant in global scope
+
+function changeLeastFavoriteCustomer() {
+  // Attempting to change the constant - this will result in an error
+  leastFavoriteCustomer = 'new value';
+}
